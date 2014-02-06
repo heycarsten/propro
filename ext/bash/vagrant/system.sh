d131c16c2bb433a730fa989a7f63b763a3640123
Carsten Nielsen <heycarsten@gmail.com>
2014-02-06 13:25:50 -0500

    Update Vagrant system install user aliases provisioner.

--- ext/bash/vagrant/system.sh
+++ ext/bash/vagrant/system.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+function vagrant-system-install-user-aliases {
+  announce "Installing helper aliases for user: $VAGRANT_USER"
+  tee -a /home/$VAGRANT_USER/.profile <<EOT
+alias be="bundle exec"
+alias r="bin/rails"
+alias v="cd $VAGRANT_DATA_DIR"
+v
+EOT
+}
+
 function provision-vagrant-system {
   section "Vagrant System"
   system-upgrade
@@ -7,4 +17,5 @@ function provision-vagrant-system {
   system-install-packages
   system-configure-shared-memory
   system-install-sources
+  vagrant-system-install-user-aliases
 }
