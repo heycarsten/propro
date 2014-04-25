183fd3589a25cd63823bf060bce21f1d6beb2718
Carsten Nielsen <heycarsten@gmail.com>
2014-04-25 17:47:41 -0400

    Updated default versions.
    Ruby: 2.0.0 -> 2.1.1
    Nginx: 1.4.7 -> 1.6.0
    Redis: 2.8.7 -> 2.8.9

--- ext/bash/app/rvm.sh
+++ ext/bash/app/rvm.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # requires app.sh
-export APP_RVM_RUBY_VERSION="2.0.0" # @specify
+export APP_RVM_RUBY_VERSION="2.1.1" # @specify
 
 function provision-app-rvm {
   rvm-install-for-user $APP_USER $APP_RVM_RUBY_VERSION
